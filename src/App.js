99afb6f7c5778fe2336cad4e42bc2b448e64dfad
smug <supersarath007@gmail.com>
2022-09-11 09:34:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name = "Vincent"
+  const message = () => "Good Morning...";
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Hello World</h1>
+      <h1>My name is {name}</h1>
+      <h2>{message()}</h2>
     </div>
   );
 }
